e1f91bc54f02ccd43e49a599e43499c767e4f70e
farhan-begg <farhan.begg@students.makeschool.com>
2023-01-06 18:01:10 -0800

    count unique values

--- index.js
+++ index.js
@@ -66,12 +66,27 @@ const sameRefactored = (array1, array2) => {
   }
   let frequencyCounter1 = {}
   let frequencyCounter2 = {}
-  for (let val of array1) {
-    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+
+  for (let i = 0; i < array1.length; i++) {
+    if (frequencyCounter1[array1[i]]) {
+      frequencyCounter1[array1[i]]++
+    } else {
+      frequencyCounter1[array1[i]] = 1
+    }
   }
-  for (let val of array2) {
-    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+  for (let i = 0; i < array2.length; i++) {
+    if (frequencyCounter2[array2[i]]) {
+      frequencyCounter2[array2[i]]++
+    } else {
+      frequencyCounter2[array2[i]] = 1
+    }
   }
+  // for (let val of array1) {
+  //   frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+  // }
+  // for (let val of array2) {
+  //   frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+  // }
 
   for (let key in frequencyCounter1) {
     if (!(key ** 2 in frequencyCounter2)) {
@@ -83,3 +98,94 @@ const sameRefactored = (array1, array2) => {
     return true
   }
 }
+
+// console.log(sameRefactored([1, 2, 4], [1, 4, 8]))
+
+
+//Anagrams 
+//Given 2 strings write a function to determine if the second string is an anagram
+
+const anagram = (string1, string2) => {
+
+  let stringOneContainer = {}
+  let stringTwoContainer = {}
+
+  if (string1.length !== string2.length) {
+    return false
+  }
+
+  for (let i = 0; i < string1.length; i++) {
+    if (stringOneContainer[string1[i]]) {
+      stringOneContainer[string1[i]]++
+
+    } else {
+      stringOneContainer[string1[i]] = 1
+    }
+  }
+
+  for (let i = 0; i < string2.length; i++) {
+    if (stringTwoContainer[string2[i]]) {
+      stringTwoContainer[string2[i]]++
+
+    } else {
+      stringTwoContainer[string2[i]] = 1
+    }
+  }
+
+  const keys1 = Object.keys(stringOneContainer);
+  const keys2 = Object.keys(stringTwoContainer);
+
+  for (let key of keys1) {
+    if (stringOneContainer[key] !== stringTwoContainer[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(anagram('test', 'tesaat'))
+// write a function called sumZero which accepts a sorted array of integers
+// function returns first pair that sum is zero
+const sumZero = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === 0) {
+        return [array[i], array[j]];
+      }
+    }
+  }
+}
+
+const sumZeroMultiplePointers = (array) => {
+  let left = 0
+  let right = array.length - 1
+  while (left < right) {
+    let sum = array[left] + array[right]
+    if (sum === 0) {
+      return [array[left], array[right]]
+    } else if (sum > 0) {
+      right--
+    } else {
+      left++
+    }
+  }
+}
+console.log(sumZeroMultiplePointers([-1, 1, 2, 3,]))
+
+
+// countUniqueValues implement a function which accepts a 
+//sorted array returns the count of unique values 
+
+const countUniqueValues = (array) => {
+
+  let i = 0;
+  for (let j = 1; j < array.length; j++) {
+    if (array[i] != array[j]) {
+      i++
+      array[i] = array[j]
+    }
+  }
+  return i + 1
+
+
+}
+console.log(countUniqueValues([-1, 1, 2, 3,]))
