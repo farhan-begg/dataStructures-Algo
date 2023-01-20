7613aa49aa031f07b21727144f0328ab1be57349
farhan-begg <farhan.begg@students.makeschool.com>
2023-01-19 22:37:22 -0800

    binary search

--- index.js
+++ index.js
@@ -209,8 +209,6 @@ const maxSubarraySums = (arr, num) => {
 }
 
 console.log(maxSubarraySums([2, 5, 6, 1, 21, 2, 1, 3], 2))
-
-
 // O(N) Time Complexity 
 const maxSubarraySum = (arr, num) => {
   let maxSum = 0;
@@ -230,3 +228,23 @@ const maxSubarraySum = (arr, num) => {
 }
 
 console.log(maxSubarraySum([2, 5, 6, 1, 21, 2, 1, 3], 2))
+
+
+//Binary Search 
+const BinarySearch = (arr, elem) => {
+  let start = 0;
+  let end = arr.length - 1
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== element) {
+    if (elem < arr[middle]) {
+      end = middle - 1
+    } else {
+      start = middle + 1
+    }
+    middle = Math.floor((start + end) / 2)
+  }
+
+
+
+}
